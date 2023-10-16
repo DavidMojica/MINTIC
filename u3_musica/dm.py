c08332be5917f420b555063dde8ca64df5c48dc1
David Mojica <davidmojicav@gmail.com>
2023-10-16 12:30:36 -0500

    u3 e5

--- u3_musica/dm.py
+++ u3_musica/dm.py
@@ -10,6 +10,16 @@ def buscar_cancion(nombre):
             return cancion
     return None
 
+def buscar_por_artista(artista):
+    canciones_artista = []
+    for cancion in musica:
+        if cancion[1] == artista:
+            canciones_artista.append(cancion)
+    return canciones_artista
+
 print(buscar_cancion("bohemian rhapsody"))
 # Print Output:
 # ('bohemian rhapsody', 'queen', 7.8)
+print(buscar_por_artista("the beatles"))
+# Print Output:
+#[('yellow submarine', 'the beatles', 9.2), ('let it be', 'the beatles', 9.9)]
