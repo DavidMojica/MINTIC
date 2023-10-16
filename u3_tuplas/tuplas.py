216bb4753819900a61d6f14180ed736c0b731a2f
David Mojica <davidmojicav@gmail.com>
2023-10-16 12:09:21 -0500

    u3 e2

---
+++ u3_tuplas/tuplas.py
@@ -0,0 +1,8 @@
+#Crear una tupla
+coordenadas = (3, 5)
+
+#Acceder a elementos
+print(coordenadas[0])
+
+#Intentar modificar una tupla (error)
+coordenadas[0] = 1 #Esto generará un error, las tuplas son inmutables.
