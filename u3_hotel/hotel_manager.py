a470c03824c86d8f88d725635b5ef62f18e7e2cf
David Mojica <davidmojicav@gmail.com>
2023-10-18 12:48:27 -0500

    save 1

--- u3_hotel/hotel_manager.py
+++ u3_hotel/hotel_manager.py
@@ -1,9 +1,17 @@
 import csv
+import os
 from hotel import Habitacion, Cliente, Reserva
 
+#Archivos planos
+habitaciones_file = 'u3_hotel/habitaciones.csv'
+clientes_file = 'u3_hotel/clientes.csv'
+reservas_file = 'u3_hotel/reservas.csv'
+
+#Vars
 habitaciones = []
 clientes = []
 reservas = []
+
 
 def get_cliente_by_index(index):
     return clientes[index]
@@ -60,39 +68,52 @@ def cancelar_reserva(cliente, habitacion):
     
     
 def guardar_datos():
-    with open('hotel.csv', mode='w', newline='') as file:
+    with open(habitaciones_file, mode='w', newline='') as file:
         writer = csv.writer(file)
-
-        # Guardar habitaciones
         for habitacion in habitaciones:
             writer.writerow(['habitacion', habitacion.numero, habitacion.tipo, habitacion.precio])
 
-        # Guardar clientes
+    with open(clientes_file, mode='w', newline='') as file:
+        writer = csv.writer(file)
         for cliente in clientes:
             writer.writerow(['cliente', cliente.nombre, cliente.correo])
 
-        # Guardar reservas
+    with open(reservas_file, mode='w', newline='') as file:
+        writer = csv.writer(file)
         for reserva in reservas:
             writer.writerow(['reserva', reserva.cliente.correo, reserva.habitacion.numero, reserva.fecha_inicio, reserva.fecha_fin])
             
 def cargar_datos():
     global habitaciones, clientes, reservas
 
-    with open('hotel.csv', mode='r') as file:
-        reader = csv.reader(file)
+    # Load habitaciones data
+    if os.path.exists(habitaciones_file):
+        with open(habitaciones_file, mode='r') as file:
+            reader = csv.reader(file)
+            for row in reader:
+                if row[0] == 'habitacion':
+                    numero, tipo, precio = map(int, row[1:])
+                    habitacion = Habitacion(numero, tipo, precio)
+                    habitaciones.append(habitacion)
 
-        for row in reader:
-            if row[0] == 'habitacion':
-                numero, tipo, precio = map(int, row[1:])
-                habitacion = Habitacion(numero, tipo, precio)
-                habitaciones.append(habitacion)
-            elif row[0] == 'cliente':
-                nombre, correo = row[1:]
-                cliente = Cliente(nombre, correo)
-                clientes.append(cliente)
-            elif row[0] == 'reserva':
-                correo_cliente, numero_habitacion, fecha_inicio, fecha_fin = row[1:]
-                cliente = next(c for c in clientes if c.correo == correo_cliente)
-                habitacion = next(h for h in habitaciones if h.numero == int(numero_habitacion))
-                reserva = Reserva(cliente, habitacion, fecha_inicio, fecha_fin)
-                reservas.append(reserva)
+    # Load clientes data
+    if os.path.exists(clientes_file):
+        with open(clientes_file, mode='r') as file:
+            reader = csv.reader(file)
+            for row in reader:
+                if row[0] == 'cliente':
+                    nombre, correo = row[1:]
+                    cliente = Cliente(nombre, correo)
+                    clientes.append(cliente)
+
+    # Load reservas data
+    if os.path.exists(reservas_file):
+        with open(reservas_file, mode='r') as file:
+            reader = csv.reader(file)
+            for row in reader:
+                if row[0] == 'reserva':
+                    correo_cliente, numero_habitacion, fecha_inicio, fecha_fin = row[1:]
+                    cliente = next(c for c in clientes if c.correo == correo_cliente)
+                    habitacion = next(h for h in habitaciones if h.numero == int(numero_habitacion))
+                    reserva = Reserva(cliente, habitacion, fecha_inicio, fecha_fin)
+                    reservas.append(reserva)
